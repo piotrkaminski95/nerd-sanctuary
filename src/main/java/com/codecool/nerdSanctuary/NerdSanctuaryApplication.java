0b900b93abb94c8eda343c26d2fdddfbd095d108
piotrkaminski95 <pkaminki95@gmail.com>
2018-12-10 11:03:54 +0100

    init project

---
+++ src/main/java/com/codecool/nerdSanctuary/NerdSanctuaryApplication.java
@@ -0,0 +1,12 @@
+package com.codecool.nerdSanctuary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NerdSanctuaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NerdSanctuaryApplication.class, args);
+	}
+}
---
+++ src/test/java/com/codecool/nerdSanctuary/NerdSanctuaryApplicationTests.java
@@ -0,0 +1,16 @@
+package com.codecool.nerdSanctuary;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NerdSanctuaryApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
