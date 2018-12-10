48690f7cba9a8705c8ba3660b02164ba187e02fc
piotrkaminski95 <pkaminki95@gmail.com>
2018-12-10 20:11:37 +0100

    add GET methods

--- src/main/java/com/codecool/nerdSanctuary/repository/GameRepository.java
+++ src/main/java/com/codecool/nerdSanctuary/repository/GameRepository.java
@@ -4,6 +4,9 @@ import com.codecool.nerdSanctuary.model.Game;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface GameRepository extends CrudRepository<Game, Long> {
+    List<Game> findAll();
 }
