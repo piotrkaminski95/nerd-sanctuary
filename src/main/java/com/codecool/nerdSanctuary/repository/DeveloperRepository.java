9260e6d59b1c5eb329c8d363fe5c43e3c60ff58e
szymslow12 <szym.slow12@gmail.com>
2018-12-11 21:00:17 +0100

    change interface to extend to JpaRepository

--- src/main/java/com/codecool/nerdSanctuary/repository/DeveloperRepository.java
+++ src/main/java/com/codecool/nerdSanctuary/repository/DeveloperRepository.java
@@ -1,9 +1,10 @@
 package com.codecool.nerdSanctuary.repository;
 
 import com.codecool.nerdSanctuary.model.Developer;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface DeveloperRepository extends CrudRepository<Developer, Long> {
+public interface DeveloperRepository extends JpaRepository<Developer, Long> {
 }
