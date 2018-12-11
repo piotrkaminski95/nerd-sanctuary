00b10376e86de22ae17f9d37a3ee70e0a69e4870
szymslow12 <szym.slow12@gmail.com>
2018-12-11 20:51:28 +0100

    add field DeveloperRepository/setter, commented DeveloperService/setter

--- src/main/java/com/codecool/nerdSanctuary/controller/DeveloperController.java
+++ src/main/java/com/codecool/nerdSanctuary/controller/DeveloperController.java
@@ -1,10 +1,26 @@
 package com.codecool.nerdSanctuary.controller;
 
+import com.codecool.nerdSanctuary.repository.DeveloperRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class DeveloperController {
 
+//    TO-DO: This class!
+//    DeveloperService service;
 
+    @Autowired
+    private DeveloperRepository repository;
+
+    public void setRepository(DeveloperRepository repository) {
+        this.repository = repository;
+    }
+
+
+    /*public void setService(DeveloperService service) {
+        this.service = service;
+    }
+     */
 
 }
