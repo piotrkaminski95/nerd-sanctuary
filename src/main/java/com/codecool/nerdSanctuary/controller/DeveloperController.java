13f410dfadea62714cd070bc60c115c0b80d0cf4
szymslow12 <szym.slow12@gmail.com>
2018-12-11 21:16:11 +0100

    change annotation @Controller to @RestController

--- src/main/java/com/codecool/nerdSanctuary/controller/DeveloperController.java
+++ src/main/java/com/codecool/nerdSanctuary/controller/DeveloperController.java
@@ -3,13 +3,13 @@ package com.codecool.nerdSanctuary.controller;
 import com.codecool.nerdSanctuary.model.Developer;
 import com.codecool.nerdSanctuary.repository.DeveloperRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@Controller
+@RestController
 public class DeveloperController {
 
 //    TO-DO: This class!
