98486a93368d7d37735fde13bd96638a238b5822
szymslow12 <szym.slow12@gmail.com>
2018-12-11 21:01:09 +0100

    add/impl getAllDevelopers

--- src/main/java/com/codecool/nerdSanctuary/controller/DeveloperController.java
+++ src/main/java/com/codecool/nerdSanctuary/controller/DeveloperController.java
@@ -1,13 +1,18 @@
 package com.codecool.nerdSanctuary.controller;
 
+import com.codecool.nerdSanctuary.model.Developer;
 import com.codecool.nerdSanctuary.repository.DeveloperRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class DeveloperController {
 
 //    TO-DO: This class!
+//    @Autowired
 //    DeveloperService service;
 
     @Autowired
@@ -23,4 +28,8 @@ public class DeveloperController {
     }
      */
 
+    @GetMapping("/developer")
+    public List<Developer> getAllDevelopers() {
+        return repository.findAll();
+    }
 }
