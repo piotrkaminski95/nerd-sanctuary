73324b70e234d2f0e7d1ca486cab45ab0ffc8d6d
piotrkaminski95 <pkaminki95@gmail.com>
2018-12-12 14:35:16 +0100

    add logger to service

--- src/main/java/com/codecool/nerdSanctuary/service/GameService.java
+++ src/main/java/com/codecool/nerdSanctuary/service/GameService.java
@@ -6,6 +6,8 @@ import com.codecool.nerdSanctuary.model.Platform;
 import com.codecool.nerdSanctuary.repository.DeveloperRepository;
 import com.codecool.nerdSanctuary.repository.GameRepository;
 import com.codecool.nerdSanctuary.repository.PlatformRepository;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -21,9 +23,12 @@ public class GameService {
     @Autowired
     private DeveloperRepository devRepo;
 
+    private Logger logger = LogManager.getLogger();
+
     public GameService() {}
 
     public List<Game> getGames() {
+        logger.info("CRUD operation: READ ALL GAMES");
         return gameRepo.findAll();
     }
 
@@ -32,7 +37,10 @@ public class GameService {
     }
 
     public List<Platform> getPlatforms(long id) {
+        logger.info(String.format("CRUD operation: READ GAME ID=%s", id));
+
         if (!gameRepo.exists(id)) {
+            logger.info(String.format("ERROR: ID=%s IS NOT EXIST!", id));
             return null;
         }
         Game game = gameRepo.findById(id);
@@ -40,7 +48,9 @@ public class GameService {
     }
 
     public Developer getDeveloper(long id) {
+        logger.info(String.format("CRUD operation: GET GAME ID=%s DEVELOPER", id));
         if (!gameRepo.exists(id)) {
+            logger.info(String.format("ERROR: ID=%s IS NOT EXIST!", id));
             return null;
         }
         Game game = gameRepo.findById(id);
@@ -48,7 +58,9 @@ public class GameService {
     }
 
     public Game addGame(Game newGame) {
+        logger.info("CRUD operation: ADD GAME");
         if (gameRepo.existsByTitle(newGame.getTitle())) {
+            logger.info(String.format("ERROR: ID=%s IS NOT EXIST!", id));
             return null;
         }
         gameRepo.save(newGame);
@@ -56,12 +68,15 @@ public class GameService {
     }
 
     public Game addPlatform(Platform platform, long id) {
+        logger.info(String.format("CRUD operation: ADD PLATFORM TO GAME ID=%s", id));
         if (!gameRepo.exists(id) || !platformRepo.existsByName(platform.getName())) {
+            logger.info(String.format("ERROR: ID=%s IS NOT EXIST!", id));
             return null;
         }
         Game game = gameRepo.findById(id);
         List<Platform> list = game.getPlatforms();
         if (list.contains(platform)) {
+            logger.info(String.format("ERROR: ID=%s IS NOT EXIST!", id));
             return null;
         }
         list.add(platform);
@@ -71,7 +86,9 @@ public class GameService {
     }
 
     public Game editGame(Game game, long id) {
+        logger.info(String.format("CRUD operation: UPDATE GAME ID=%s", id));
         if (!gameRepo.exists(id)) {
+            logger.info(String.format("ERROR: ID=%s IS NOT EXIST!", id));
             return null;
         }
         Game oldGame = gameRepo.findById(id);
@@ -83,12 +100,15 @@ public class GameService {
     }
 
     public Game editGamePlatforms(List<Platform> platforms, long id) {
+        logger.info(String.format("CRUD operation: UPDATE GAME ID=%s PLATFORM LIST", id));
         if (!gameRepo.exists(id)) {
+            logger.info(String.format("ERROR: ID=%s IS NOT EXIST!", id));
             return null;
         }
 
         for (Platform p : platforms) {
             if (!platformRepo.existsByName(p.getName())) {
+                logger.info(String.format("ERROR: PLATFORM=%s IS NOT EXIST!", p.getName()));
                 return null;
             }
         }
@@ -100,11 +120,14 @@ public class GameService {
     }
 
     public Game editDeveloper(Developer developer, long id) {
+        logger.info(String.format("CRUD operation: UPDATE GAME ID=%s DEVELOPER", id));
         if (!gameRepo.exists(id)) {
+            logger.info(String.format("ERROR: ID=%s IS NOT EXIST!", id));
             return null;
         }
 
         if (!devRepo.existsByName(developer.getName())) {
+            logger.info(String.format("ERROR: DEVELOPER=%s IS NOT EXIST!", developer.getName()));
             return null;
         }
 
@@ -115,7 +138,9 @@ public class GameService {
     }
 
     public Game deleteGame(long id) {
+        logger.info(String.format("CRUD operation: DELETE GAME ID=%s", id));
         if (!gameRepo.exists(id)) {
+            logger.info(String.format("ERROR: ID=%s IS NOT EXIST!", id));
             return null;
         }
 
