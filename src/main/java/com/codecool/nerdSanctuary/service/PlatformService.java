e378451a8be23840e4cd19e4f416c34faf097fc0
szymslow12 <szym.slow12@gmail.com>
2018-12-12 17:15:11 +0100

    add/impl getPlatform

--- src/main/java/com/codecool/nerdSanctuary/service/PlatformService.java
+++ src/main/java/com/codecool/nerdSanctuary/service/PlatformService.java
@@ -21,4 +21,13 @@ public class PlatformService {
         logger.info("CRUD operation: READ ALL PLATFORMS");
         return repository.findAll();
     }
+
+
+    public Platform getPlatform(long id) {
+        logger.info(String.format("CRUD operation: READ PLATFORM ID=%s", id));
+        if (repository.exists(id)) {
+            return repository.findOne(id);
+        }
+        throw new IllegalArgumentException(String.format("ID=%s does not exist", id));
+    }
 }
