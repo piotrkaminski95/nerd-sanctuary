0488d840d872a0ec1e8abb2b65adf5c4ec645f51
szymslow12 <szym.slow12@gmail.com>
2018-12-12 17:09:03 +0100

    change extended interface to JpaRepository

--- src/main/java/com/codecool/nerdSanctuary/repository/PlatformRepository.java
+++ src/main/java/com/codecool/nerdSanctuary/repository/PlatformRepository.java
@@ -1,11 +1,11 @@
 package com.codecool.nerdSanctuary.repository;
 
 import com.codecool.nerdSanctuary.model.Platform;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PlatformRepository extends CrudRepository<Platform, Long> {
+public interface PlatformRepository extends JpaRepository<Platform, Long> {
     boolean existsByName(String name);
 
     Platform findByName(String name);
