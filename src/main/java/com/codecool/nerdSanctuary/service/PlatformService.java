fd26367bf6b6016b95bc3318c0051f01ad795c21
szymslow12 <szym.slow12@gmail.com>
2018-12-12 17:09:48 +0100

    add/impl getAllPlatform

--- src/main/java/com/codecool/nerdSanctuary/service/PlatformService.java
+++ src/main/java/com/codecool/nerdSanctuary/service/PlatformService.java
@@ -1,10 +1,13 @@
 package com.codecool.nerdSanctuary.service;
 
+import com.codecool.nerdSanctuary.model.Platform;
 import com.codecool.nerdSanctuary.repository.PlatformRepository;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class PlatformService {
@@ -14,5 +17,8 @@ public class PlatformService {
 
     private Logger logger = LogManager.getLogger();
 
-
+    public List<Platform> getAllPlatform() {
+        logger.info("CRUD operation: READ ALL PLATFORMS");
+        return repository.findAll();
+    }
 }
