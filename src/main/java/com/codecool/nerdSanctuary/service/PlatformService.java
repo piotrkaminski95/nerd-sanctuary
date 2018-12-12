83494e26d18043095f1b6c4e1271656f8e5d14aa
szymslow12 <szym.slow12@gmail.com>
2018-12-12 17:07:55 +0100

    add field logger

--- src/main/java/com/codecool/nerdSanctuary/service/PlatformService.java
+++ src/main/java/com/codecool/nerdSanctuary/service/PlatformService.java
@@ -1,6 +1,8 @@
 package com.codecool.nerdSanctuary.service;
 
 import com.codecool.nerdSanctuary.repository.PlatformRepository;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -9,4 +11,8 @@ public class PlatformService {
 
     @Autowired
     private PlatformRepository repository;
+
+    private Logger logger = LogManager.getLogger();
+
+
 }
