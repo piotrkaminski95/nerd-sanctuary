c494b5cd4c58eed2580f40b4f007d6ad261021c3
szymslow12 <szym.slow12@gmail.com>
2018-12-13 14:38:42 +0100

    add field JavaMailSender, refactor handleRunTimeException

--- src/main/java/com/codecool/nerdSanctuary/controller/ExceptionController.java
+++ src/main/java/com/codecool/nerdSanctuary/controller/ExceptionController.java
@@ -2,19 +2,30 @@ package com.codecool.nerdSanctuary.controller;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import javax.servlet.http.HttpServletResponse;
+import java.util.stream.Stream;
 
 @ControllerAdvice
 public class ExceptionController {
 
     private Logger logger = LogManager.getLogger();
 
+    @Autowired
+    private JavaMailSender javaMailSender;
+
     @ExceptionHandler(RuntimeException.class)
     public RuntimeException handleRunTimeException(RuntimeException exception, HttpServletResponse response) {
-        logger.error(getMessage(exception, response));
+        String message = getMessage(exception, response);
+        logger.error(message);
+        if (response.getStatus() == 500) {
+            sendEmail(exception, message);
+        }
         return exception;
     }
 
@@ -27,4 +38,7 @@ public class ExceptionController {
                 exception.getMessage()
         );
     }
+
+
+
 }
