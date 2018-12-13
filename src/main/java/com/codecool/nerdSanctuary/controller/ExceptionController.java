54211266d9817e8ed8f7d49472a1c46054c21ac1
szymslow12 <szym.slow12@gmail.com>
2018-12-13 13:51:30 +0100

    rename handleResourceNotFound -> handleRunTimeException

--- src/main/java/com/codecool/nerdSanctuary/controller/ExceptionController.java
+++ src/main/java/com/codecool/nerdSanctuary/controller/ExceptionController.java
@@ -1,6 +1,5 @@
 package com.codecool.nerdSanctuary.controller;
 
-import com.codecool.nerdSanctuary.exceptions.ResourceNotFoundException;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -11,9 +10,9 @@ public class ExceptionController {
 
     private Logger logger = LogManager.getLogger();
 
-    @ExceptionHandler(ResourceNotFoundException.class)
-    public ResourceNotFoundException handleResourceNotFound(ResourceNotFoundException exception) {
-        logger.error(exception.toString() + " - " + exception.getMessage());
+    @ExceptionHandler(RuntimeException.class)
+    public RuntimeException handleRunTimeException(RuntimeException exception) {
+        logger.error(exception.getClass().getSimpleName() + " - " + exception.getMessage());
         return exception;
     }
 }
