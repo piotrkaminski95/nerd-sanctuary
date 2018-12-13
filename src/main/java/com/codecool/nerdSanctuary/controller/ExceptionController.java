fc302fc4e6f8147b95d0bbeecdf462c3f838bd7f
szymslow12 <szym.slow12@gmail.com>
2018-12-13 13:58:00 +0100

    add parameter HttpServletResponse to handleRunTimeException

--- src/main/java/com/codecool/nerdSanctuary/controller/ExceptionController.java
+++ src/main/java/com/codecool/nerdSanctuary/controller/ExceptionController.java
@@ -5,14 +5,16 @@ import org.apache.logging.log4j.Logger;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import javax.servlet.http.HttpServletResponse;
+
 @ControllerAdvice
 public class ExceptionController {
 
     private Logger logger = LogManager.getLogger();
 
     @ExceptionHandler(RuntimeException.class)
-    public RuntimeException handleRunTimeException(RuntimeException exception) {
-        logger.error(exception.getClass().getSimpleName() + " - " + exception.getMessage());
+    public RuntimeException handleRunTimeException(RuntimeException exception, HttpServletResponse response) {
+        logger.error(exception.getClass().getSimpleName() + "- CODE= " + response.getStatus() + " - " + exception.getMessage());
         return exception;
     }
 }
